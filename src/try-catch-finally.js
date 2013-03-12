868f7334e167ae7473a89270897007a1530b77ca
Chris Watson <chris.watson@7digital.com>
2013-03-12 17:32:12 +0000

    Remove all global namespacing - only export as module

--- src/try-catch-finally.js
+++ src/try-catch-finally.js
@@ -1,60 +1,59 @@
-(function preventLeaks() {
+define(function defineTryCatchFinally() {
 
-	function namespaceTo(ns) {
+	function TryCatch(tryBlock) {
 
-		function TryCatch(tryBlock) {
-			var errorToHandle;
-			try {
-				tryBlock();
-			}
-			catch (e) {
-				errorToHandle = e;
-			}
-			this['catch'] = function (errorType, handleError) {
-				if (errorNotHandled() && arguments.length > 0) {
-					if (isUndefined(handleError)) {
-						handleError = errorType;
-						errorType = undefined;
-						handleError(errorToHandle);
-						setErrorHandled();
-					}
-					else if (errorToBeHandledIsType(errorType)) {
-						handleError(errorToHandle);
-						setErrorHandled();
-					}
-				}
-				return this;
-			};
-			this['finally'] = function (callback) {
-				if(callback) callback();
-				if (errorNotHandled()) throw errorToHandle;
-			};
-			function setErrorHandled() { errorToHandle = undefined; }
-			function isUndefined(subject) { return typeof subject === 'undefined'; }
-			function errorNotHandled() { return !isUndefined(errorToHandle); }
-			function errorToBeHandledIsType(errorType) {
-				if (typeof errorType === 'string') {
-					return errorToHandle.constructor.name === errorType
-						|| new RegExp('^\\s*function ' + errorType + '()').test(errorToHandle.constructor.toString()); // for IE
-				}
-				return errorToHandle instanceof errorType
-					|| errorToHandle.constructor.name === errorType.name;
-			}
+		var errorToHandle;
+
+		try {
+			tryBlock();
+		}
+		catch (e) {
+			errorToHandle = e;
 		}
 
-		ns['try'] = function (tryBlock) {
-			return new TryCatch(tryBlock);
+		this['catch'] = function (errorType, handleError) {
+			if (errorNotHandled() && arguments.length > 0) {
+				if (isUndefined(handleError)) {
+					handleError = errorType;
+					errorType = undefined;
+					handleError(errorToHandle);
+					setErrorHandled();
+				}
+				else if (errorToBeHandledIsType(errorType)) {
+					handleError(errorToHandle);
+					setErrorHandled();
+				}
+			}
+			return this;
 		};
 
+		this['finally'] = function (callback) {
+			if(callback) callback();
+			if (errorNotHandled()) throw errorToHandle;
+		};
+
+		function setErrorHandled() { errorToHandle = undefined; }
+
+		function isUndefined(subject) { return typeof subject === 'undefined'; }
+
+		function errorNotHandled() { return !isUndefined(errorToHandle); }
+
+		function errorToBeHandledIsType(errorType) {
+
+			if (typeof errorType === 'string') {
+				return errorToHandle.constructor.name === errorType
+					|| new RegExp('^\\s*function ' + errorType + '()').test(errorToHandle.constructor.toString()); // for IE
+			}
+
+			return errorToHandle instanceof errorType
+				|| errorToHandle.constructor.name === errorType.name;
+
+		}
+		
 	}
 
-	if (typeof define === "function" && define.amd) {
-		define(function exportAsDefine() {
-			var exportObj = {};
-			namespaceTo(exportObj);
-			return exportObj['try'];
-		});
-	}
-	else namespaceTo(window.c24w = window.c24w || {});
+	return function _try(tryBlock) {
+		return new TryCatch(tryBlock);
+	};
 
-})();
+});
--- test/test.js
+++ test/test.js
@@ -2,102 +2,106 @@ define(['./lib/chai', '../src/try-catch-finally'], function tryCatchFinallyTests
 
 	var expect = chai.expect;
 
-	describe('try', function () {
+		describe('require', function () {
 
-		it('should exist', function () {
-			expect(_try).to.be.a('function');
-		});
+		describe('try', function () {
 
-		it('should be chainable', function () {
-			expect(_try()).to.be.an('object');
-		});
-
-		it('should execute the function passed to try', function (done) {
-			_try(done);
-		});
-
-	});
-
-	describe('catch', function () {
-
-		it('should exist', function () {
-			expect(_try().catch).to.be.a('function');
-		});
-
-		it('should be chainable', function () {
-			expect(_try().catch(Object, function () {})).to.be.an('object');
-		});
-
-		it('should catch TypeError if try block is undefined', function (done) {
-			_try()
-			.catch(TypeError, function (e) {
-				expect(e.message).to.match(/'?undefined'?|'?tryBlock'? is not a function|Object expected/); // 'Safari' Chrome | 'Opera' Firefox | IE
-				done();
-			});
-		});
-
-		it('should be ignored if arguments are undefined', function () {
-			expect(_try().catch());
-		});
-
-		describe('literal object', function () {
-
-			it('with indiscriminate catch', function (done) {
-				function tryBlock() { throw 'blah'; }
-				_try(tryBlock)
-				.catch(function (e) {
-					expect(e).to.be.a('string');
-					done();
-				});
+			it('should exist', function () {
+				expect(_try).to.be.a('function');
 			});
 
-			it('by constructor', function (done) {
-				function tryBlock() { throw 12345; }
-				_try(tryBlock)
-				.catch(Number, function (e) {
-					expect(e).to.be.a('number');
-					done();
-				});
+			it('should be chainable', function () {
+				expect(_try()).to.be.an('object');
 			});
 
-			it('by name', function (done) {
-				function tryBlock() { throw [1,2,3]; }
-				_try(tryBlock)
-				.catch('Array', function (e) {
-					expect(e).to.be.an('array');
-					done();
-				});
+			it('should execute the function passed to try', function (done) {
+				_try(done);
 			});
 
 		});
 
-		describe('constucted object', function () {
+		describe('catch', function () {
 
-			it('with indiscriminate catch', function (done) {
-				function tryBlock() { throw new Error(); }
-				_try(tryBlock)
-				.catch(function (e) {
-					expect(e.name).to.equal('Error');
+			it('should exist', function () {
+				expect(_try().catch).to.be.a('function');
+			});
+
+			it('should be chainable', function () {
+				expect(_try().catch(Object, function () {})).to.be.an('object');
+			});
+
+			it('should catch TypeError if try block is undefined', function (done) {
+				_try()
+				.catch(TypeError, function (e) {
+					expect(e.message).to.match(/'?undefined'?|'?tryBlock'? is not a function|Object expected/); // 'Safari' Chrome | 'Opera' Firefox | IE
 					done();
 				});
 			});
 
-			it('by constructor', function (done) {
-				function tryBlock() { throw new RegExp(); }
-				_try(tryBlock)
-				.catch(RegExp, function (e) {
-					expect(e).to.be.a('regexp');
-					done();
-				});
+			it('should be ignored if arguments are undefined', function () {
+				expect(_try().catch());
 			});
 
-			it('by name', function (done) {
-				function tryBlock() { throw new Number(12345); }
-				_try(tryBlock)
-				.catch('Number', function (e) {
-					expect(e).to.be.a('number');
-					done();
+			describe('literal object', function () {
+
+				it('with indiscriminate catch', function (done) {
+					function tryBlock() { throw 'blah'; }
+					_try(tryBlock)
+					.catch(function (e) {
+						expect(e).to.be.a('string');
+						done();
+					});
 				});
+
+				it('by constructor', function (done) {
+					function tryBlock() { throw 12345; }
+					_try(tryBlock)
+					.catch(Number, function (e) {
+						expect(e).to.be.a('number');
+						done();
+					});
+				});
+
+				it('by name', function (done) {
+					function tryBlock() { throw [1,2,3]; }
+					_try(tryBlock)
+					.catch('Array', function (e) {
+						expect(e).to.be.an('array');
+						done();
+					});
+				});
+
+			});
+
+			describe('constucted object', function () {
+
+				it('with indiscriminate catch', function (done) {
+					function tryBlock() { throw new Error(); }
+					_try(tryBlock)
+					.catch(function (e) {
+						expect(e.name).to.equal('Error');
+						done();
+					});
+				});
+
+				it('by constructor', function (done) {
+					function tryBlock() { throw new RegExp(); }
+					_try(tryBlock)
+					.catch(RegExp, function (e) {
+						expect(e).to.be.a('regexp');
+						done();
+					});
+				});
+
+				it('by name', function (done) {
+					function tryBlock() { throw new Number(12345); }
+					_try(tryBlock)
+					.catch('Number', function (e) {
+						expect(e).to.be.a('number');
+						done();
+					});
+				});
+
 			});
 
 		});
