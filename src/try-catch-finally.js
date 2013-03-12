4e155f2804c2a37b1035d7f4efe14a41c738bd6d
Chris Watson <chris.watson@7digital.com>
2013-03-12 16:54:13 +0000

    Add require export compatibility to t-c-f and use require in tests

--- src/try-catch-finally.js
+++ src/try-catch-finally.js
@@ -1,47 +1,60 @@
-(function (c24w) {
+(function preventLeaks() {
 
-	function TryCatch(tryBlock) {
-		var errorToHandle;
-		try {
-			tryBlock();
-		}
-		catch (e) {
-			errorToHandle = e;
-		}
-		this['catch'] = function (errorType, handleError) {
-			if (errorNotHandled() && arguments.length > 0) {
-				if (isUndefined(handleError)) {
-					handleError = errorType;
-					errorType = undefined;
-					handleError(errorToHandle);
-					setErrorHandled();
-				}
-				else if (errorToBeHandledIsType(errorType)) {
-					handleError(errorToHandle);
-					setErrorHandled();
-				}
+	function namespaceTo(ns) {
+
+		function TryCatch(tryBlock) {
+			var errorToHandle;
+			try {
+				tryBlock();
 			}
-			return this;
-		};
-		this['finally'] = function (callback) {
-			if(callback) callback();
-			if (errorNotHandled()) throw errorToHandle;
-		};
-		function setErrorHandled() { errorToHandle = undefined; }
-		function isUndefined(subject) { return typeof subject === 'undefined'; }
-		function errorNotHandled() { return !isUndefined(errorToHandle); }
-		function errorToBeHandledIsType(errorType) {
-			if (typeof errorType === 'string') {
-				return errorToHandle.constructor.name === errorType
-					|| new RegExp('^\\s*function ' + errorType + '()').test(errorToHandle.constructor.toString()); // for IE
+			catch (e) {
+				errorToHandle = e;
 			}
-			return errorToHandle instanceof errorType
-				|| errorToHandle.constructor.name === errorType.name;
+			this['catch'] = function (errorType, handleError) {
+				if (errorNotHandled() && arguments.length > 0) {
+					if (isUndefined(handleError)) {
+						handleError = errorType;
+						errorType = undefined;
+						handleError(errorToHandle);
+						setErrorHandled();
+					}
+					else if (errorToBeHandledIsType(errorType)) {
+						handleError(errorToHandle);
+						setErrorHandled();
+					}
+				}
+				return this;
+			};
+			this['finally'] = function (callback) {
+				if(callback) callback();
+				if (errorNotHandled()) throw errorToHandle;
+			};
+			function setErrorHandled() { errorToHandle = undefined; }
+			function isUndefined(subject) { return typeof subject === 'undefined'; }
+			function errorNotHandled() { return !isUndefined(errorToHandle); }
+			function errorToBeHandledIsType(errorType) {
+				if (typeof errorType === 'string') {
+					return errorToHandle.constructor.name === errorType
+						|| new RegExp('^\\s*function ' + errorType + '()').test(errorToHandle.constructor.toString()); // for IE
+				}
+				return errorToHandle instanceof errorType
+					|| errorToHandle.constructor.name === errorType.name;
+			}
 		}
+
+		ns['try'] = function (tryBlock) {
+			return new TryCatch(tryBlock);
+		};
+
 	}
 
-	c24w['try'] = function (tryBlock) {
-		return new TryCatch(tryBlock);
-	};
+	if (typeof define === "function" && define.amd) {
+		define(function exportAsDefine() {
+			var exportObj = {};
+			namespaceTo(exportObj);
+			return exportObj['try'];
+		});
+	}
+	else namespaceTo(window.c24w = window.c24w || {});
 
-})(window.c24w = window.c24w || {});
+})();
