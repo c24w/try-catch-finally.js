1a30829bfca8339bc72c6c96b6cfea6b55503140
Chris Watson <chris.watson@7digital.com>
2013-03-18 12:20:38 +0000

    Refactor tests

--- test/catch-fail.test.js
+++ test/catch-fail.test.js
@@ -4,6 +4,14 @@ define(['chai', 'tcf'], function tryCatchFinallyTests(chai, _try) {
 
 	describe('catch', function () {
 
+		function assert_catch_specific_fail(toThrow, toCatch) {
+			function throwsString(){ throw toThrow; }
+
+			function handleError() { throw 'expected this not to execute'; }
+
+			_try(throwsString).catch(toCatch, handleError);
+		}
+
 		it('should not call catch error handler if try block does not throw', function () {
 			function doesNotThrow(){}
 
@@ -13,27 +21,15 @@ define(['chai', 'tcf'], function tryCatchFinallyTests(chai, _try) {
 		});
 
 		it('should not call catch error handler if catch by name doesn\'t match thrown error', function () {
-			function throwsString(){ throw 'Literal string'; }
-
-			function handleError() { throw 'expected this not to execute'; }
-
-			_try(throwsString).catch('Strin', handleError);
+			assert_catch_specific_fail('Literal string', 'Strin');
 		});
 
 		it('should not call catch error handler if catch by constructor doesn\'t match thrown error', function () {
-			function throwsString(){ throw []; }
-
-			function handleError() { throw 'expected this not to execute'; }
-
-			_try(throwsString).catch(String, handleError);
+			assert_catch_specific_fail([], String);
 		});
 
 		it('should not call catch error handler if catch by value doesn\'t match thrown error', function () {
-			function throwsString(){ throw 12345; }
-
-			function handleError() { throw 'expected this not to execute'; }
-
-			_try(throwsString).catch(54321, handleError);
+			assert_catch_specific_fail(12345, 1234);
 		});
 
 	});
