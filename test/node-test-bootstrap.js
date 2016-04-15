3ece0361b379cee7ff0b391196089e95b6e4faaa
Chris Watson <chris24watson@gmail.com>
2016-04-15 21:23:31 +0100

    Use lodash assign
    So test work in node < 4

--- test/node-test-bootstrap.js
+++ test/node-test-bootstrap.js
@@ -1,11 +1,13 @@
 'use strict';
+var assign = require('lodash.assign');
+
 // Simulate browser globals for running tests in node
 global.chai = require('chai');
 global._try = require('../try-catch-finally');
 
 // Patch mocha colours
 // https://github.com/mochajs/mocha/issues/1200
-Object.assign(require('mocha/lib/reporters/base').colors, {
+assign(require('mocha/lib/reporters/base').colors, {
   'pass': 92,
   'error stack': 92,
   'fast': 92,
