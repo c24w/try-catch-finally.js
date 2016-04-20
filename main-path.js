a2eb8b5f8f36ffe0df24836e36051a52095e91fb
Chris Watson <chris24watson@gmail.com>
2016-04-20 22:53:58 +0100

    Don't use es6!
    Still testing in node 0.10Remove

--- main-path.js
+++ main-path.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node 
 'use strict';
-/* jshint esversion: 6 */
-const pkg = require('./package.json');
-const min = process.env.MINIFIED;
-const main = min ? pkg.main.replace('.js', '.min.js') : pkg.main;
-const path = `${__dirname}/${main}`;
+var path = require('path');
+var pkg = require('./package.json');
+var min = process.env.MINIFIED;
+var mainFile = min ? pkg.main.replace('.js', '.min.js') : pkg.main;
+var mainPath = path.join(__dirname, mainFile);
 
 if (require.main === module) { // Called directly
-  process.stdout.write(path);
+  process.stdout.write(mainPath);
 } else { // Required
-  module.exports = path;
+  module.exports = mainPath;
 }
