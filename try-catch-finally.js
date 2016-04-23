aab422849cde8d0a51a98450c7f3883e975a5fde
Travis CI <chris24watson@gmail.com>
2016-04-23 22:33:08 +0100

    Update(?) umd amd definition
    That's how jQuery do it 😁

--- try-catch-finally.js
+++ try-catch-finally.js
@@ -2,7 +2,7 @@
   'use strict';
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module.
-    define([], factory);
+    define('try-catch-finally', [], factory);
   } else if (typeof module === 'object' && module.exports) {
     // Node. Does not work with strict CommonJS, but
     // only CommonJS-like environments that support module.exports,
