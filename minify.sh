2a480873874b5611f0c1bcd50d76d0a1e94860a3
Chris Watson <chris24watson@gmail.com>
2018-03-17 18:30:17 +0000

    Fix minify script not adding info line comment

--- minify.sh
+++ minify.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
+
 PRE='// try-catch-finally.js | (c) Chris Watson | http://git.io/tcf'
 SRC=$(./main-path.js)
 DST=$(MINIFIED=y ./main-path.js)
-node_modules/.bin/uglifyjs -cm --preamble "$PRE" "$SRC" -o "$DST"
+
+MIN=$(node_modules/.bin/uglifyjs "$SRC" -cm)
+
+echo "$PRE" > "$DST"
+echo "$MIN" >> "$DST"
