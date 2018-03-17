5f149840c349f031bb87b9279fdd020959bc5875
Chris Watson <chris24watson@gmail.com>
2018-03-17 19:04:09 +0000

    Refactor commit minified script

--- commit-minified.sh
+++ commit-minified.sh
@@ -5,8 +5,7 @@ function die { echo "$2" && exit $1; }
 
 git checkout master # Get out of detached head state
 git fetch origin master # Get latest
-git diff --quiet --exit-code HEAD..origin/master ||\
-  die 0 'Master changed since build start' 
+git diff --quiet HEAD..origin/master || die 0 'Master changed since build start'
 
 MIN_FILE=$(MINIFIED=y ./main-path.js short)
 
@@ -14,8 +13,7 @@ git config user.name 'Travis CI'
 git config user.email deploy@travis-ci.org
 
 git add "$MIN_FILE"
-git diff --cached --quiet --exit-code "$MIN_FILE" &&\
-  die 0 "$MIN_FILE unchanged"
+git diff --cached --quiet "$MIN_FILE" && die 0 "$MIN_FILE unchanged"
 
 ./build-readme.sh
 git add readme.md
@@ -25,5 +23,4 @@ MSG=$(git log -1 --pretty="Build $MIN_FILE @%h%n%n- %B%n$UGLIFY")
 git commit -m "$MSG"
 # -q and output redirects prevent leaking the token!
 AUTHED_ORIGIN=https://$GH_TOKEN@github.com/c24w/try-catch-finally.js.git
-git push -q "$AUTHED_ORIGIN" master > /dev/null 2>&1 ||\
-  die 1 'Push failed'
+git push -q "$AUTHED_ORIGIN" master > /dev/null 2>&1 || die 1 'Push failed'
